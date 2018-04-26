8a55a9a612ac8ca551f8147c17d8702bf29e8d36
Robert Zeng <1093968009@qq.com>
2018-04-26 22:20:42 +0800

    add product attributor

--- application/common/model/Productsattributes.php
+++ application/common/model/Productsattributes.php
@@ -8,6 +8,10 @@ class Productsattributes extends Model
 {
 	 protected $table = TABLE_PRODUCTSATTRIBUTES;
 	 
-	
-	
+	/**
+	 * 通过产品id获取产品属性
+	 * */
+	public  function getproattr($id){
+		Db::table($this->table)->where('products_id',$id)->select();
+	}
 }
