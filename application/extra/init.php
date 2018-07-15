cb4e749f51044aa77eac6cc11492c761d9c36a61
Robert Zeng <1093968009@qq.com>
2018-07-15 21:25:25 +0800

    change env function

--- application/extra/init.php
+++ application/extra/init.php
@@ -4,7 +4,7 @@ use think\Env;
 define('CHARSET', 'utf-8');
 define('HTTPS_SERVER', 'https://'.Env::get('site.domain'));
 define('HTTP_SERVER', 'http://'.Env::get('site.domain'));
-define('CDNDOMAIN', 'https://img.sellart-online.com/');
+define('CDNDOMAIN', Env::get('site.cdndomain'));
 define('DIR_WS_IMAGES', 'images/');
-define('ENABLE_SSL', true);
+define('ENABLE_SSL', Env::get('site.ssl'));
 
